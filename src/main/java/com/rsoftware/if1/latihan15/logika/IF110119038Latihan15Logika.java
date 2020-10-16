ce6fed2dda04eb23f6b0ea368012c757beb1a37a
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 16:32:24 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan15/logika/IF110119038Latihan15Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan15.logika;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : Logika
+ * 
+ */
+
+public class IF110119038Latihan15Logika {
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
